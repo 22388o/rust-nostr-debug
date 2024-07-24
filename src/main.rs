1b890e03f79daf7b049c2d9e3e9ebb25d6a12ef0
reya <123083837+reyamir@users.noreply.github.com>
2024-07-24 07:13:56 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -10,28 +10,29 @@ async fn main() -> Result<()> {
     client.connect().await;
 
     let before = Instant::now();
+    let _ = get_profile(&client).await;
+    println!("Elapsed time: {:.2?}", before.elapsed());
 
-    // let _ = get_profile(&client).await;
-    let _ = get_profile_slow(&client).await;
+    client.add_relay("wss://relay.nostr.net").await?;
+    let _ = client.connect_relay("wss://relay.nostr.net").await;
+    println!("Added relay .1");
 
+    let before = Instant::now();
+    let _ = get_profile(&client).await;
+    println!("Elapsed time: {:.2?}", before.elapsed());
+
+    client.add_relay("wss://relay.primal.net").await?;
+    let _ = client.connect_relay("wss://relay.primal.net").await;
+    println!("Added relay .2");
+
+    let before = Instant::now();
+    let _ = get_profile(&client).await;
     println!("Elapsed time: {:.2?}", before.elapsed());
 
     Ok(())
 }
 
 async fn get_profile(client: &Client) -> Result<()> {
-    let test_pk =
-        PublicKey::from_bech32("npub1zfss807aer0j26mwp2la0ume0jqde3823rmu97ra6sgyyg956e0s6xw445")?;
-    let filter = Filter::new().author(test_pk).kind(Kind::Metadata).limit(1);
-    let _ = client.get_events_of(vec![filter], None).await?;
-
-    Ok(())
-}
-
-async fn get_profile_slow(client: &Client) -> Result<()> {
-    client.add_relay("wss://relay.nostr.net").await?;
-    client.connect_relay("wss://relay.nostr.net").await?;
-
     let test_pk =
         PublicKey::from_bech32("npub1zfss807aer0j26mwp2la0ume0jqde3823rmu97ra6sgyyg956e0s6xw445")?;
     let filter = Filter::new().author(test_pk).kind(Kind::Metadata).limit(1);
