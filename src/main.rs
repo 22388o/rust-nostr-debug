273934f1e5d2b0382b8146e4d5b4ee92ed272ca1
reya <123083837+reyamir@users.noreply.github.com>
2024-07-25 17:35:06 +0700

    auth not work

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,36 @@
 use nostr_sdk::prelude::*;
-use std::time::Instant;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let keys: Keys = Keys::generate();
-    let client = Client::new(&keys);
+    let signer = NostrSigner::Keys(keys);
+
+    // Config
+    let opts = Options::new().automatic_authentication(true);
+
+    // Setup nostr client
+    let client = ClientBuilder::default().signer(signer).opts(opts).build();
 
     client.add_relay("wss://relay.damus.io").await?;
     client.connect().await;
 
-    let before = Instant::now();
-    let _ = get_profile(&client).await;
-    println!("Elapsed time: {:.2?}", before.elapsed());
+    // Add nip-17 relay
+    client.add_relay("wss://auth.nostr1.com").await?;
+    client.connect_relay("wss://auth.nostr1.com").await?;
 
-    client.add_relay("wss://relay.nostr.net").await?;
-    let _ = client.connect_relay("wss://relay.nostr.net").await;
-    println!("Added relay .1");
+    // Send private message
+    let receiver =
+        PublicKey::from_bech32("npub1zfss807aer0j26mwp2la0ume0jqde3823rmu97ra6sgyyg956e0s6xw445")
+            .unwrap();
+    let message = "Hello world";
 
-    let before = Instant::now();
-    let _ = get_profile(&client).await;
-    println!("Elapsed time: {:.2?}", before.elapsed());
-
-    client.add_relay("wss://relay.primal.net").await?;
-    let _ = client.connect_relay("wss://relay.primal.net").await;
-    println!("Added relay .2");
-
-    let before = Instant::now();
-    let _ = get_profile(&client).await;
-    println!("Elapsed time: {:.2?}", before.elapsed());
-
-    Ok(())
-}
-
-async fn get_profile(client: &Client) -> Result<()> {
-    let test_pk =
-        PublicKey::from_bech32("npub1zfss807aer0j26mwp2la0ume0jqde3823rmu97ra6sgyyg956e0s6xw445")?;
-    let filter = Filter::new().author(test_pk).kind(Kind::Metadata).limit(1);
-    let _ = client.get_events_of(vec![filter], None).await?;
+    match client
+        .send_private_msg_to(["wss://auth.nostr1.com"], receiver, message, None)
+        .await
+    {
+        Ok(output) => println!("Ok: {:?}", output),
+        Err(e) => println!("Err: {}", e),
+    }
 
     Ok(())
 }
